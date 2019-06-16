d9bd0bbfad42e0cb51299a9e16dfb9a62141a55b
Filippo Paolini <filippo.paolini@gmail.com>
2019-06-16 16:56:37 +0200

    TileMap 2.1
    minor error fixed

--- Def.h
+++ Def.h
@@ -8,7 +8,7 @@
 #define TILE_SET_TEXTURE "assets/mapkmn.png"
 #define MAP_WIDTH 71
 #define MAP_HEIGHT 36
-#define TILE_SIZE sf::Vector2u(175, 175)
+#define TILE_SIZE sf::Vector2u(75, 75)
 #define MAP_1 "../assets/Map1.txt"
 #define MAP_2 "../assets/Map2.txt"
 #define MAP_3 "../assets/Map3.txt"
